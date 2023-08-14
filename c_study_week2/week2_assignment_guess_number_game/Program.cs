f1d5e54a3390dd88bdcc003dbde92b03413620e9
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-14 18:04:29 +0900

    C# 문법 종합반 2주차 과제 완료

---
+++ c_study_week2/week2_assignment_guess_number_game/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace week2_assignment_guess_number_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNumber = new Random().Next(1, 101);
+            int playerNum = 0;
+            Console.WriteLine("1부터 100 사이의 숫자를 맞혀보세요!");
+            while (playerNum != targetNumber)
+            {
+                Console.Write("숫자를 입력하세요: ");
+                playerNum = int.Parse(Console.ReadLine());
+                if (playerNum < targetNumber)
+                {
+                    Console.WriteLine("더 큰 숫자입니다.");
+                }
+                else if (playerNum > targetNumber)
+                {
+                    Console.WriteLine("더 작은 숫자입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다!");
+                }
+            }
+
+
+            Console.WriteLine("1부터 9 사이의 숫자 3가지를 맞혀보세요!");
+
+            int[] targetNumbers = new int[3];
+            for (int i = 0; i < targetNumbers.Length; i++)
+            {
+                targetNumbers[i] = new Random().Next(1, 10);
+            }
+
+            while (true)
+            {
+                Console.Write("숫자 3개를 입력하세요: ");
+                string[] answer = Console.ReadLine().Split(' ');
+                int[] playerNums = new int[3];
+                for (int i = 0; i < playerNums.Length; i++)
+                {
+                    playerNums[i] = int.Parse(answer[i]);
+                }
+
+                int correct = 0;
+                for (int i = 0; i < targetNumbers.Length; i++)
+                {
+                    for (int j = 0; j < playerNums.Length; j++)
+                    {
+                        if (targetNumbers[i] == playerNums[j])
+                        {
+                            correct++;
+                            break;
+                        }
+                    }
+                }
+
+                if (correct == 3)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("{0}개 맞혔습니다.", correct);
+                }
+            }
+        }
+    }
+}
---
+++ c_study_week2/week2_assignment_tic_tac_toe/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace week2_assignment_tic_tac_toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[3, 3];
+
+            int gameOver = 0;
+            while (gameOver == 0)
+            {
+                Console.Write("Player1 : ");
+                string[] player1Choice = Console.ReadLine().Split(',');
+                int player1_x = int.Parse(player1Choice[0]);
+                int player1_y = int.Parse(player1Choice[1]);
+                board[player1_x, player1_y] = 1;
+
+                Console.Write("Player2 : ");
+                string[] player2Choice = Console.ReadLine().Split(',');
+                int player2_x = int.Parse(player2Choice[0]);
+                int player2_y = int.Parse(player2Choice[1]);
+                board[player2_x, player2_y] = 2;
+
+                printGameBoard(board);
+
+                gameOver = isGameOver(board);
+                if (gameOver == 1)
+                {
+                    Console.WriteLine("Player1 승리!");
+                }
+                else if (gameOver == 2)
+                {
+                    Console.WriteLine("Player2 승리!");
+                }
+            }
+        }
+
+        static void printGameBoard(int[,] gameBoard)
+        {
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameBoard.GetLength(1); j++)
+                {
+                    if (gameBoard[i, j] == 1)
+                    {
+                        Console.Write("O");
+                    } else if (gameBoard[i, j] == 2)
+                    {
+                        Console.Write("X");
+                    } else
+                    {
+                        Console.Write(".");
+                    }
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int isGameOver(int[,] gameBoard)
+        {
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
+            {
+                int correct = 0;
+                int cur = gameBoard[i, 0];
+                for (int j = 1; j < gameBoard.GetLength(1); j++)
+                {
+                    if (gameBoard[i,j] == cur){
+                        correct++;
+                    } else
+                    {
+                        break;
+                    }
+                }
+                if (correct == 2)
+                {
+                    return cur;
+                }
+            }
+
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
+            {
+                int correct = 0;
+                int cur = gameBoard[0, i];
+                for (int j = 1; j < gameBoard.GetLength(1); j++)
+                {
+                    if (gameBoard[j, i] == cur)
+                    {
+                        correct++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (correct == 2)
+                {
+                    return cur;
+                }
+            }
+
+            if (
+                (gameBoard[0, 0] == gameBoard[1, 1]) && (gameBoard[0, 0] == gameBoard[2, 2]) ||
+                (gameBoard[2, 0] == gameBoard[1, 1]) && (gameBoard[2, 0] == gameBoard[0, 2])
+            ) return gameBoard[1, 1];
+
+            return 0;
+        }
+    }
+}
