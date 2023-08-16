ef182664e62a14f7a1781f027446c0f700ea86e3
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-16 15:50:11 +0900

    C# 문법종합반 4-1 수강

---
+++ c_study_week4/c_study_week4_4_1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace c_study_week4_4_1
+{
+    internal class Program
+    {
+        public interface IMovable
+        {
+            void Move(int x, int y);
+        }
+
+        public class Player : IMovable
+        {
+            public void Move(int x, int y) 
+            { 
+
+            }
+        }
+
+        public class Enemy : IMovable
+        {
+            public void Move(int x, int y)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IMovable movableObject1 = new Player();
+            IMovable movableObject2 = new Enemy();
+
+            movableObject1.Move(1, 2);
+            movableObject2.Move(2, 3);
+        }
+    }
+}
---
+++ c_study_week4/c_study_week4_4_1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace c_study_week4_4_1_2
+{
+    internal class Program
+    {
+        public interface IUsable
+        {
+            void Use();
+        }
+
+        public class Item : IUsable 
+        { 
+            public string Name { get; set; }
+
+            public void Use()
+            {
+                Console.WriteLine("아이템 {0}를 사용했습니다.", Name);
+            }
+        }
+
+        public class Player
+        {
+            public void UserItem(IUsable item)
+            {
+                item.Use();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Item item = new Item() { Name = "Health Potion" };  // 매개변수가 아님. 초기화를 위해 값을 미리 세팅한 것
+            player.UserItem(item);
+        }
+    }
+}
