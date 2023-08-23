9c930bae3aa8f64c48fe288591bff59f1d720f1a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-23 13:32:03 +0900

    C# 문법종합반 5-2 강의 정리

---
+++ c_study_week5/c_study_week5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace c_study_week5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 2, 4, 6, 1, 3 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ c_study_week5/c_study_week5_2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace c_study_week5_2
+{
+    internal class Program
+    {
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp; 
+        }
+
+        static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, right);
+
+            return i + 1;
+        }
+
+        static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                // 2, 5, 4, 6, 1, 3
+                // 2, 1, 4, 6, 5, 3
+                // 2, 1, 3, 6, 5, 4
+                // 첫 pivot 인덱스는 2가 됨 (pivot 값 3)
+
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 2, 4, 6, 1, 3 };
+
+            QuickSort(arr, 0, arr.Length - 1);
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
