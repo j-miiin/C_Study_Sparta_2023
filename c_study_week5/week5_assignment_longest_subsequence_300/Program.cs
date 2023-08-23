4cc14a4ed9c6a6126e32b34f4a8c300de5d3559c
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-23 21:01:44 +0900

    C# 문법종합반 5주차 과제 - longest increasing subsequence 300

---
+++ c_study_week5/week5_assignment_longest_subsequence_300/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace week5_assignment_longest_subsequence_300
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.LengthOfLIS(new int[] { 0, 1, 0, 3, 2, 3 }));
+        }
+
+        public class Solution
+        {
+            public static int LengthOfLIS(int[] nums)
+            {
+                int[] longestSub = new int[nums.Length];
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    longestSub[i] = 1;
+                    for (int j = 0; j <= i - 1; j++)
+                    {
+                        if ((nums[j] < nums[i]) && (longestSub[j] + 1 > longestSub[i]))
+                        {
+                            longestSub[i] = longestSub[j] + 1;
+                        }
+                    }
+                }
+
+                return longestSub.Max();
+            }
+        }
+    }
+}
