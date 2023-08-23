a58c5dca9743e54e031b4810f90e288bc194e3c6
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-23 15:41:33 +0900

    C# 문법종합반 5-3, 5-4 정리

---
+++ c_study_week5/c_study_week5_3/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace c_study_week5_3
+{
+    internal class Program
+    {
+        public class Graph
+        {
+            private int V;
+            private List<int>[] adj;
+
+            public Graph(int V)
+            {
+                this.V = V;
+                adj = new List<int>[V];
+                for (int i = 0; i < V; i++)
+                {
+                    adj[i] = new List<int>();
+                }
+            }
+
+            public void AddEdge(int v, int w)
+            {
+                adj[v].Add(w);
+            }
+
+            public void DFS(int v)
+            {
+                bool[] visited = new bool[V];
+                DFSUtil(v, visited);
+            }
+
+            private void DFSUtil(int v, bool[] visited)
+            {
+                visited[v] = true;
+                Console.Write($"{v} ");
+
+                foreach (int n in adj[v])
+                {
+                    if (!visited[n])
+                    {
+                        DFSUtil(n, visited);
+                    }
+                }
+            }
+
+            public void BFS(int v)
+            {
+                bool[] visited = new bool[V];
+                Queue<int> queue = new Queue<int> ();
+
+                visited[v] = true;
+                queue.Enqueue(v);
+
+                while (queue.Count > 0)
+                {
+                    int n = queue.Dequeue();
+                    Console.Write($"{n} ");
+
+                    foreach (int m in adj[n])
+                    {
+                        if (!visited[m])
+                        {
+                            visited[m] = true;
+                            queue.Enqueue(m);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(6);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(3, 5);
+            graph.AddEdge(4, 5);
+
+            Console.WriteLine("DFS travelsal: ");
+            graph.DFS(0);
+            Console.WriteLine();
+
+            Console.WriteLine("BFS travelsal: ");
+            graph.BFS(0);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ c_study_week5/c_study_week5_3_2/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace c_study_week5_3_2
+{
+    internal class Program
+    {
+        static int V = 6;
+
+        static void Dijkstra(int[,] graph, int start)
+        {
+            int[] distance = new int[V];
+            bool[] visited = new bool[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                distance[i] = int.MaxValue;
+            }
+
+            distance[start] = 0;
+
+            for (int count = 0; count < V - 1; count++)
+            {
+                int minDistance = int.MaxValue;
+                int minIndex = -1;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!visited[v] && distance[v] <= minDistance)
+                    {
+                        minDistance = distance[v];
+                        minIndex = v;
+                    }
+                }
+
+                visited[minIndex] = true;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!visited[v] && graph[minIndex, v] != 0 
+                        && distance[minIndex] != int.MaxValue && distance[minIndex] + graph[minIndex, v] < distance[v])
+                    {
+                        distance[v] = distance[minIndex] + graph[minIndex, v];
+                    }
+                }
+            }
+
+            Console.WriteLine("정점\t거리");
+            for (int i = 0; i < V; i++)
+            {
+                Console.WriteLine($"{i}\t{distance[i]}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] graph = {
+            { 0, 4, 0, 0, 0, 0 },
+            { 4, 0, 8, 0, 0, 0 },
+            { 0, 8, 0, 7, 0, 4 },
+            { 0, 0, 7, 0, 9, 14 },
+            { 0, 0, 0, 9, 0, 10 },
+            { 0, 0, 4, 14, 10, 0 }
+        };
+
+            int start = 0; // 시작 정점
+
+            Dijkstra(graph, start);
+        }
+    }
+}
